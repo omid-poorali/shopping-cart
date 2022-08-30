da6ca70f09e4d20cf068f11051af572484386e91
toranj-org <omidpoorali1992@gmail.com>
2022-08-30 15:09:50 +0430

    fix: clip-path property

--- src/components/icons/time.tsx
+++ src/components/icons/time.tsx
@@ -1,7 +1,7 @@
 export const Time = () => {
     return (
         <svg width="15" height="14" viewBox="0 0 15 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <g clip-path="url(#clip0_4_25)">
+            <g clipPath="url(#clip0_4_25)">
                 <path d="M6.13672 13.3334L4.47308 11.7734L9.89354 11.7867C12.3276 11.7867 14.3254 9.83335 14.3254 7.44002C14.3254 6.24668 13.8276 5.16003 13.0231 4.37337" stroke="#AAB7C7" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
                 <path d="M5.45453 7.33333H9.54544" stroke="#AAB7C7" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
                 <path d="M5.1549 2.65332C2.72081 2.65332 0.723083 4.60667 0.723083 7C0.723083 8.19333 1.22081 9.27998 2.02536 10.0667" stroke="#AAB7C7" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
